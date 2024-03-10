d283db03dfd6726a5817a4408240bfb9c9b03adc
Simon Lydell <simon.lydell@gmail.com>
2024-03-10 13:50:09 +0100

    Try a smaller script

--- test.js
+++ test.js
@@ -1,67 +1,35 @@
 const pty = require("node-pty");
 
-// Run `child.js` 5 times, passing the exit code we want from it and after how long time in milliseconds.
-const configurations = [
-  { exitCode: 0, runTime: 700 },
-  { exitCode: 0, runTime: 700 },
-  { exitCode: 0, runTime: 2000 },
-  { exitCode: 0, runTime: 700 },
-  { exitCode: 1, runTime: 700 },
-];
+let buffer1 = "";
+const terminal1 = pty.spawn("sleep", ["1"], {});
+terminal1.onData((data) => {
+  buffer1 += data;
+});
+terminal1.onExit(({ exitCode, signal }) => {
+  console.log("\nExit 1", { exitCode, signal });
+  console.log(buffer1);
+  if (signal === 1) {
+    console.log("✅ Got the expected signal 1");
+  } else {
+    console.log("❌ Expected signal 1, got", signal);
+    process.exitCode = 1;
+  }
+});
 
-const queue = configurations.map((configuration, i) =>
-  run(i + 1, configuration)
-);
+let buffer2 = "";
+const terminal2 = pty.spawn("sleep", ["1"], {});
+terminal2.onData((data) => {
+  buffer2 += data;
+});
+terminal2.onExit(({ exitCode, signal }) => {
+  console.log("\nExit 2", { exitCode, signal });
+  console.log(buffer2);
+  if (signal === 0) {
+    console.log("✅ Got the expected signal 0");
+  } else {
+    console.log("❌ Expected signal 0, got", signal);
+    process.exitCode = 1;
+  }
+});
 
-const receivedExitCodes = [];
-
-function run(i, configuration) {
-  return () => {
-    console.log("Start", i);
-
-    let buffer = "";
-
-    const terminal = pty.spawn(
-      "node",
-      [
-        "child.js",
-        configuration.exitCode.toString(),
-        configuration.runTime.toString(),
-      ],
-      {}
-    );
-
-    terminal.onData((data) => {
-      buffer += data;
-    });
-
-    terminal.onExit(({ exitCode, signal }) => {
-      console.log("Exit", i, { exitCode, signal });
-      console.log(buffer);
-      receivedExitCodes.push(exitCode);
-
-      const next = queue.shift();
-      if (next !== undefined) {
-        // Start the next in the queue, if there are any left.
-        next();
-      } else if (receivedExitCodes.length === configurations.length) {
-        // When done, see if we got the expected output.
-        console.log("receivedExitCodes", receivedExitCodes);
-        const isExpected =
-          receivedExitCodes.filter((code) => code === 1).length === 1;
-        if (isExpected) {
-          console.log("✅ Expected output");
-          process.exit(0);
-        } else {
-          console.log("🚨 Unexpected output. Expected one single exit code 1.");
-          process.exit(1);
-        }
-      }
-    });
-  };
-}
-
-// Start running, 2 at a time.
-for (let i = 0; i < 2; i++) {
-  queue.shift()?.();
-}
+terminal1.kill();
